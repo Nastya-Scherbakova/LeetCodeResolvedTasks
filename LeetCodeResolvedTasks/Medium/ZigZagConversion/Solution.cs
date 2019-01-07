ad690a3c3255c1fd80b0917e87757bc4675ecdf5
Nastya Scherbakova <nshcherbakovaa@gmail.com>
2019-01-07 14:15:21 +0200

    zigzag conversion medium task

---
+++ LeetCodeResolvedTasks/Medium/ZigZagConversion/Solution.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/zigzag-conversion/
+
+namespace LeetCodeResolvedTasks.Medium.ZigZagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            string result = "";
+            for (int i = 0; i < numRows; i++)
+            {
+                var letterSpacing = (numRows - i - 1 + numRows - i - 2) < 0 ? numRows - 1 + numRows - 2 : numRows - i - 1 + numRows - i - 2;
+                for (int j = i; j < s.Length; j += letterSpacing + 1)
+                {
+                    result += s[j];
+                    if (numRows == 1)
+                    {
+                        result = s;
+                        j = s.Length;
+                    }
+                    if (i != 0 && i != numRows - 1 && j != i)
+                    {
+                        j += i + 1 * i;
+                        if (j < s.Length)
+                        {
+                            result += s[j];
+                        }
+                    }
+
+                }
+            }
+            return result;
+        }
+    }
+}
--- LeetCodeResolvedTasks/Program.cs
+++ LeetCodeResolvedTasks/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// you only need to import namespace with task that you need
 using LeetCodeResolvedTasks.Medium.LongestPalindromicSubstring;
 using System;
 
@@ -8,6 +8,7 @@ namespace LeetCodeResolvedTasks
     {
         static void Main(string[] args)
         {
+            // example of using (create solution object and use it's method)
             Solution task = new Solution();
             Console.WriteLine(task.LongestPalindrome("ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"));
             Console.ReadLine();
