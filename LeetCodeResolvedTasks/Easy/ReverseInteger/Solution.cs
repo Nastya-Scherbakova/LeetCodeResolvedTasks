24c7218137bfab1b834f8ca7aea670eb0acf3545
Nastya Scherbakova <nshcherbakovaa@gmail.com>
2019-01-07 14:44:21 +0200

    reverse int, faster then 100% solutions on C# in LeetCode

---
+++ LeetCodeResolvedTasks/Easy/ReverseInteger/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+// https://leetcode.com/problems/reverse-integer/
+
+namespace LeetCodeResolvedTasks.Easy.ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            try
+            {
+                var z = checked(handleReverse(x));
+                return z;
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+        }
+
+        private static int handleReverse(int x)
+        {
+            bool negative = false;
+            if (x < 0)
+            {
+                negative = true;
+                x = checked(x * -1);
+            }
+            string number = x.ToString();
+            return negative ? ReverseToInt(number) * -1 : ReverseToInt(number);
+        }
+        public static int ReverseToInt(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            var result = new string(charArray);
+            return int.Parse(result);
+        }
+
+    }
+}
